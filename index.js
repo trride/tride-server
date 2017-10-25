bc13f91164e279bf5a98ba592a53feb86044c9b6
rama841 <kickyou44@gmail.com>
2017-10-25 20:47:58 +0700

    combine get prices

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
+require("dotenv").config();
 const { send } = require("micro");
 const { router, get } = require("microrouter");
 
+// gojek
+const { GojekHandler } = require("gojek-handler");
+const gojek = new GojekHandler({
+  authorization: process.env.gojek_token
+});
+
+// grab
+const GrabHandler = require("tride-grab-handler");
+const grab = new GrabHandler(process.env.grab_token);
+
+// uber
+const UberHandler = require("uber-handler");
+const uber = new UberHandler({
+  token: process.env.uber_token
+});
+
 const tride = async (req, res) => {
-  await new Promise(r => setTimeout(r, 1000)); // example async/await
-  send(res, 200, { message: "Hello from Tride API" });
+  const payload = {
+    start: {
+      lat: +req.query.start_lat || 0,
+      long: +req.query.start_long || 0
+    },
+    end: {
+      lat: +req.query.end_lat || 0,
+      long: +req.query.end_long || 0
+    }
+  };
+
+  const gojekPrice = gojek.getMotorBikePrice(payload.start, payload.end);
+  const grabPrice = grab.getMotorBikePrice(payload.start, payload.end);
+  const uberPrice = uber.getPrice(payload.start, payload.end);
+  const allPrices = await Promise.all([gojekPrice, grabPrice, uberPrice]);
+
+  send(res, 200, {
+    gojek: allPrices[0],
+    grab: allPrices[1],
+    uber: allPrices[2]
+  });
 };
 
 const notFound = (req, res) => send(res, 404, "Route not found.");
